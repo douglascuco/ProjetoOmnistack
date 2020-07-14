a04a8e36449724dee688c6305a7b29834b595399
Douglas Cuco <douglas.cuco@hbsis.com.br>
2020-07-14 18:02:51 -0300

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.connect(
-    'mongodb+srv://cuco:cuco@semanaomnistack-1w77s.gcp.mongodb.net/semanaOmni?retryWrites=true&w=majority', {
+    'mongodb+srv://cuco:gUYvjeZxeNgbp1F1@semanaomnistack-1w77s.gcp.mongodb.net/semanaOmni?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
